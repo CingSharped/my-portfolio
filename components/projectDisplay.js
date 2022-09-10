4f2006e10ac6dff59cbc240fc032b9b5a7b01ced
CingSharped <chrismsharpe123@gmail.com>
2022-09-10 09:15:53 +0100

    Initial Commit

---
+++ components/projectDisplay.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from '../styles/Home.module.css'
+
+const ProjectDisplay = ({project}) => {
+    return (
+        <div className={styles.card}>
+            <Image src={project.Image} width="450px" height="300px" />
+            <Link href={project.Link}>
+                <h2>{project.Title}</h2>
+            </Link>
+            <p>{project.Description}</p>
+                {project.Skills.split(',').map((skill, i) => {
+                    <footer key={i} className="blockquote-footer">{skill}</footer>
+                })}
+        </div>
+    )
+}
+
+export default ProjectDisplay
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['scontent-man2-1.xx.fbcdn.net', 'img.itch.zone', 'scontent-lhr8-1.xx.fbcdn.net'],
+  }
 }
 
 module.exports = nextConfig
