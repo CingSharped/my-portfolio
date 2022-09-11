bc2a46290b56eb67b323d31e8a119eaea864812d
CingSharped <chrismsharpe123@gmail.com>
2022-09-11 18:11:00 +0100

    get initial props

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import ProjectDisplay from '../components/projectDisplay'
 
-export const getServerSideProps = async (ctx) => {
+export const getInitialProps = async (ctx) => {
   let dev = process.env.NODE_ENV !== 'production'
   let server = dev ? 'http://localhost:3000/' : 'my-portfolio-tau-beryl.vercel.app'
   const url = (server + 'api/projects')
